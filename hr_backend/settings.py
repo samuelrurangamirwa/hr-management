24317416e51c698ecf205a0d5835c2ef754fb5f2
samuelrurangamirwa <rurangamirwa@gmail.com>
2025-12-03 11:38:37 +0200

    Add frontend url on backend

--- hr_backend/settings.py
+++ hr_backend/settings.py
@@ -146,6 +146,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://hr-management-pearl-gamma.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
